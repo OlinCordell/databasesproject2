cb11a2bf5c54b38bd46216743e4c80858ef346c3
Olin Cordell <74875952+OlinCordell@users.noreply.github.com>
2025-10-20 11:11:37 -0400

    Update database_setup.sql

--- database_setup.sql
+++ database_setup.sql
@@ -11,9 +11,46 @@ create table if not exists user (
     password varchar(255) not null,
     firstName varchar(255) not null,
     lastName varchar(255) not null,
+    lastActiveDate datetime,
+    profileImagePath varchar(255),
     primary key (userId),
     unique (username),
     constraint userName_min_length check (char_length(trim(userName)) >= 2),
     constraint firstName_min_length check (char_length(trim(firstName)) >= 2),
     constraint lastName_min_length check (char_length(trim(lastName)) >= 2)
+);
+
+-- Create the post table
+create table if not exists post (
+    postId varchar(255),
+    content varchar(255) not null,
+    postDate datetime not null,
+    user int not null,
+    heartsCount int not null default 0,
+    commentsCount int not null default 0,
+    isHearted boolean not null default FALSE,
+    isBookmarked boolean not null default FALSE,
+    primary key (postId),
+    foreign key (user) references user(userId)
+);
+
+-- Create the comment table
+create table if not exists comment (
+    commentId int auto_increment,
+    postId varchar(255) not null,
+    content varchar(255) not null,
+    postDate datetime not null,
+    userId int not null,
+    primary key (commentId),
+    foreign key (postId) references post(postId),
+    foreign key (userId) references user(userId)
+);
+
+-- Create the follows table
+create table if not exists follows (
+    followsId int not null,
+    followedId int not null,
+    primary key (followsId, followedId),
+    foreign key (followsId) references user(userId) on delete cascade,
+    foreign key (followedId) references user(userId) on delete cascade
 );
