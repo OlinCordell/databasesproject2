6cdf2875899bb62736b65e5f025d482c67714184
Olin Cordell <74875952+OlinCordell@users.noreply.github.com>
2025-10-20 11:35:05 -0400

    Update PeopleService.java

--- src/main/java/uga/menik/csx370/services/PeopleService.java
+++ src/main/java/uga/menik/csx370/services/PeopleService.java
@@ -6,38 +6,151 @@ This is a project developed by Dr. Menik to give the students an opportunity to 
 package uga.menik.csx370.services;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
 
 import uga.menik.csx370.models.FollowableUser;
-import uga.menik.csx370.utility.Utility;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * This service contains people related functions.
  */
 @Service
 public class PeopleService {
+
+    // dataSource enables talking to the database.
+    private final DataSource dataSource;
+
+    /**
+     * See AuthInterceptor notes regarding dependency injection and
+     * inversion of control.
+     */
+    @Autowired
+    public PeopleService(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     
     /**
      * This function should query and return all users that 
      * are followable. The list should not contain the user 
      * with id userIdToExclude.
      */
-    public List<FollowableUser> getFollowableUsers(String userIdToExclude) {
-        // Write an SQL query to find the users that are not the current user.
+    public List<FollowableUser> getFollowableUsers(String userIdToExclude) throws SQLException {
+        
+        // Finds users that are not the currently logged in user.
+        final String sql = """
+                            select u.userId, u.firstName, u.lastName, u.lastActiveDate, u.profileImagePath,
+                                case when f.followedId is not null then true else false end as isFollowed
+                            from user u
+                            left join follows f on u.userId = f.followedId and f.followsId = ?
+                            where u.userId <> ?
+                            """;
 
-        // Run the query with a datasource.
-        // See UserService.java to see how to inject DataSource instance and
-        // use it to run a query.
+        List<FollowableUser> followableUsers = new ArrayList<>();
+       
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            
+            pstmt.setString(1, userIdToExclude);
+            pstmt.setString(2, userIdToExclude);
 
-        // Use the query result to create a list of followable users.
-        // See UserService.java to see how to access rows and their attributes
-        // from the query result.
-        // Check the following createSampleFollowableUserList function to see 
-        // how to create a list of FollowableUsers.
+            try (ResultSet rs = pstmt.executeQuery()) {
 
-        // Replace the following line and return the list you created.
-        return Utility.createSampleFollowableUserList();
+                while (rs.next()) {
+                    String userId = rs.getString("userId");
+                    String firstName = rs.getString("firstName");
+                    String lastName = rs.getString("lastName");
+                    boolean isFollowed = rs.getBoolean("isFollowed");
+                    String lastActiveDate = rs.getString("lastActiveDate");
+                    if (lastActiveDate == null) {
+                        lastActiveDate = "N/A";
+                    }
+
+                    followableUsers.add(new FollowableUser(
+                        userId, firstName, lastName, isFollowed, lastActiveDate));
+                    
+                }
+            } 
+        } catch (SQLException e) {
+                e.printStackTrace();
+        }
+
+        return followableUsers;
+    }
+
+    public void followUser(String followsId, String followedId) throws SQLException {
+        final String sql = """
+                insert into follows (followsId, followedId) values (?, ?)
+        """;
+        final String updateSql  = """
+                UPDATE user
+                SET lastActive = CURRENT_TIMESTAMP
+                WHERE userId = ?
+        """;
+        try (Connection conn = dataSource.getConnection()) {
+            conn.setAutoCommit(false);
+
+            try  (PreparedStatement pstmt = conn.prepareStatement(sql);
+                PreparedStatement updateStmt = conn.prepareStatement(updateSql)) {
+                    pstmt.setString(1, followsId);
+                    pstmt.setString(2, followedId);
+                    pstmt.executeUpdate();
+
+                    updateStmt.setString(1, followsId);
+                    updateStmt.executeUpdate();
+
+                    conn.commit();
+            } catch (Exception e) {
+                conn.rollback();
+                throw e;
+            } finally {
+                conn.setAutoCommit(true);
+            }
+        }
+    }
+
+    public void unfollowUser(String followsId, String followedId) throws SQLException {
+        final String sql = """
+            DELETE FROM follows
+            WHERE followsId = ? AND followedId = ?
+        """;
+
+        final String updateSql = """
+            UPDATE user
+            SET lastActive = CURRENT_TIMESTAMP
+            WHERE userId = ?
+        """;
+
+        try (Connection conn = dataSource.getConnection()) {
+            conn.setAutoCommit(false);
+
+            try (PreparedStatement stmt = conn.prepareStatement(sql);
+                PreparedStatement updateStmt = conn.prepareStatement(updateSql)
+            ) {
+                stmt.setString(1, followsId);
+                stmt.setString(2, followedId);
+                stmt.executeUpdate();
+
+                updateStmt.setString(1, followsId);
+                updateStmt.executeUpdate();
+
+                conn.commit();
+            } catch (Exception e) {
+                conn.rollback();
+                throw e;
+            } finally {
+                conn.setAutoCommit(true);
+            }
+        }
     }
 
 }
