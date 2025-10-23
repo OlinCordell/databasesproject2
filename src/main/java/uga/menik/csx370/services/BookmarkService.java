2b07cf2f2eeb2535ce6aa55d2c63893af4b8903a
ugacs-mmadd <168934677+ugacs-mmadd@users.noreply.github.com>
2025-10-23 13:07:56 -0400

    Create BookmarkService.java

---
+++ src/main/java/uga/menik/csx370/services/BookmarkService.java
@@ -0,0 +1,39 @@
+package uga.menik.csx370.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import uga.menik.csx370.models.Post;
+import uga.menik.csx370.repo.BookmarkRepo;
+
+@Service
+public class BookmarkService {
+
+    private final BookmarkRepo repo;
+
+    public BookmarkService(BookmarkRepo repo) {
+        this.repo = repo;
+    } // BookmarkService 
+
+    public List<Post> getBookmarkedPosts(int userId) {
+        return repo.findAllBMs(userId);
+    } // getBookmarkedPosts
+
+    @Transactional
+    public boolean ctrlBookmark(int userId, String postId) {
+        boolean already = repo.isBM(userId, postId);
+        if (already) {
+            repo.removeBM(userId, postId);
+            return false;
+        } else {
+            repo.addBM(userId, postId);
+            return true;
+        } // if else
+    } // ctrlBookmark
+
+    public boolean isBookmarked(int userId, String postId) {
+        return repo.isBM(userId, postId);
+    } // isBookmarked
+} // BookmarkService
